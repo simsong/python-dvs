e891fdf63cf66a963f2e74ee6e5d4d625f7ba047
Simson L. Garfinkel <simson.l.garfinkel@census.gov>
2020-09-03 14:58:05 -0400

    initial test

--- tests/dvsserver_test.py
+++ tests/dvsserver_test.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-
 import os
 import sys
 import urllib.parse
 import urllib.request
-sys.path.append( os.path.join( os.path.dirname(__file__), '..'))
-import webmaint
-import ctools
-import ctools.dbfile as dbfile
-
+import logging
+import warnings
 import pytest
-
 """
 Test programs for the dvsserver both without and with the bottle server.
 """
+def test_first():
+    try:
+        import webmaint
+    except ImportError:
+        warnings.warn("Cannot run first_test without webmaint")
+        return
