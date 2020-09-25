9b2bd7491953fcc82a64d28edba0a951576bf491
Simson L. Garfinkel <simson.l.garfinkel@census.gov>
2020-09-25 15:55:48 -0400

    minor refactoring

--- dvs/__init__.py
+++ dvs/__init__.py
@@ -1,18 +1,21 @@
 import time
+import warnings
 
+
+from .dvs_constants import *
 
 class DVS():
     def __init__(self, *args, **kwargs):
         """Start a DVS transaction"""
-        self.commit = kwargs
+        self.the_commit = kwargs
         self.t0     = time.time()
         pass
 
     def add(self, which, *args, **kwargs):
         """Basic method for adding an object to one of the lists """
         assert which in [COMMIT_BEFORE, COMMIT_METHOD, COMMIT_AFTER]
-        if which not in self.commit:
-            self.commit[which] = list()
+        if which not in self.the_commit:
+            self.the_commit[which] = list()
 
 
     def add_before(self, *args, **kwargs):
@@ -24,9 +27,12 @@ class DVS():
     def add_after(self, *args, **kwargs):
         return self.add(COMMIT_AFTER, *args, **kwargs)
 
+    def commit(self, *args, **kwargs):
+        warnings.warn("commit not implemented yet")
+
+
 
 """
 TODO: Take logic in dvs.py/do_commit_send and move here.
 TODO: remake dvs.py so that it uses the DVS module.
 """
-assert False
--- dvs/server.py
+++ dvs/server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-dvsserver
+dvs server:
+Functions that support the server.
 
 """
 
